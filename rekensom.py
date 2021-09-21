907190d9dd53e6de21eda0a92e4ebecc2a731413
nickvdt <32934@ma-web.nl>
2021-09-21 12:10:09 +0200

    eerste commit

---
+++ rekensom.py
@@ -0,0 +1,31 @@
+#sommen
+getal1 = 5678 
+getal2 = 4321
+getal3 = 93
+getal4 = 13
+getal5 = 99
+print(getal1 + getal2)
+print(getal3 * getal4 - getal5) 
+
+#String
+StringA = ("Developers")
+print(StringA[4:10])
+
+#Firetruck
+a = ("Firetruck")
+print("f" + a[6:9])
+
+#Naam
+print("Enter your name")
+x = input()
+print("Hello, ", x)
+
+#Rekenmachine
+print("Tel twee getallen bij elkaar op")
+x = input()
+y = input()
+z = int(x)+int(y)
+print(z)
+
+
+
