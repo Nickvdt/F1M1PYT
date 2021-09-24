916b763bb55dcff888158b0c0dfb6fed60bfc09e
Nick <32934@ma-web.nl>
2021-09-24 10:30:23 +0200

    5 keuzes toegevoed aan het python document.

---
+++ PYTB1L3DailyChoices/PYTB1L3DailyChoices.py
@@ -0,0 +1,50 @@
+vraag1 = input("Je wekker gaat, ga je opstaan of druk je op snooze"+ ('\n') )
+if vraag1.lower() == "opstaan":
+    print("Je staat op en je gaat eten maken" + ('\n') )
+elif vraag1.lower() == "snooze":
+    print("Je valt weer in slaap maar wordt 15 min later wakker. Ik zou maar eten gaan maken voordat ik te laat ben." + ('\n') )
+else:
+    print("ongeldig antwoord")
+
+vraag2 = input("Wat ga je klaarmaken, broodje hagelslag of kaas"+ ('\n') )
+if vraag2.lower() == "kaas":
+    print("Je hebt een broodje kaas klaargemaakt" + ('\n') )
+elif vraag2.lower() == "hagelslag":
+    print("Hagelslag valt op de grond moet je dat ook nog op ruimen pfff" + ('\n') )
+else:
+    print("ongeldig antwoord")
+
+vraag3 = input("Je moet nog je hond uitlaten ga je de riem meenemen ja of nee?"+ ('\n') )
+if vraag3.lower() == "ja":
+    print("je hebt net je hond uitgelaten" + ('\n') )
+elif vraag3.lower() == "nee":
+    print("Ohh jeej je hond loopt weg dit kost je 5min van je tijd" + ('\n') )
+else:
+    print("ongeldig antwoord")
+
+
+vraag4 = input("Hoe ga je vandaag naar school met de trein of fiets?"+ ('\n') )
+if vraag4.lower() == "fiets":
+    print("NEE NEE NEE NEEE KLAPBAND dan maar later de trein nemen." + ('\n') )
+elif vraag4.lower() == "trein":
+    print("Je bent optijd :D Kedeng kedeng kedeng kedeng kedeng Oe oe" + ('\n') )
+else:
+    print("ongeldig antwoord")
+
+vraag5 = input("Je bent net uitgestapt uit de trein, wat ga je nu doen: Lopen of de metro nemen?" + ('\n'))
+if vraag5.lower() == "lopen":
+        print("Helaas je bent te laat op school" + ('\n'))
+elif vraag5.lower() == "metro":
+        print("Je bent net optijd op school :D" + ('\n'))
+else:
+    print("ongeldig antwoord")
+
+#code gemaakt door Nick
+
+
+
+
+    
+
+
+
