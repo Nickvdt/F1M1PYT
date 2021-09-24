8696f1c7999075e233e51f5fac7977e8cb90e2b7
Nick <32934@ma-web.nl>
2021-09-24 09:13:03 +0200

    4 regels in het gedicht gemaakt.

---
+++ PYTB1L2PoemPrinting/gedicht.py
@@ -0,0 +1,6 @@
+vers1 = "Mijn naam is Nick van der tol"
+vers2 = "Ik zit nu te programmeren op school"
+vers3 = "Mijn hond heet Bella en zat nooit op een hondenschool"
+vers4 = "en ik speel graag op mijn console"
+
+print(vers1,vers2, vers3, vers4)
